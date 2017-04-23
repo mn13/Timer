f9fc3da14ff1fb5ef240eddcf55b1cb89973d936
nma <mndvl0p@gmail.com>
2017-04-23 16:04:16 +0300

    initial project version

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace MyTimer
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string time;
+        public string Time
+        {
+            get { return time; }
+            set {
+                time = value.Trim();
+                OnPropertyChanged("Time");
+            }
+        }
+        private bool topmost;
+        public bool Topmost
+        {
+            get { return topmost; }
+            set
+            {
+                topmost = value;
+                OnPropertyChanged("Topmost");
+            }
+        }
+        private bool isEnable;
+        public bool IsEnable
+        {
+            get { return isEnable; }
+            set
+            {
+                isEnable = value;
+                OnPropertyChanged("IsEnable");
+            }
+        } //доступен текстбокс
+        private bool stop;
+        public bool Stop
+        {
+            get { return stop; }
+            set
+            {
+                stop = value;
+                OnPropertyChanged("Stop");
+            }
+        }
+        private System.Windows.WindowState windowState;
+        public System.Windows.WindowState WindowState
+        {
+            get { return windowState; }
+            set
+            {
+                windowState = value;
+                OnPropertyChanged("WindowState");
+            }
+        }
+        MyTimerClass timerObj;
+        //System.Diagnostics.Stopwatch anotherTimer;
+        public MainWindowViewModel()
+        {
+            timerObj = new MyTimerClass(
+                (param)=>
+                {
+                    Time = param.ToString();
+                },
+                ()=>
+                {
+                    WindowState = System.Windows.WindowState.Normal; //таймер завершил работу
+                    Topmost = true;
+                    IsEnable = true;
+                    Stop = true;
+                    //цвет?
+                });
+            IsEnable = true;
+            Stop = false;
+        }
+        protected void OnPropertyChanged(string PropertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+        StartCommand _startCommand;
+        public StartCommand startCommand
+        {
+            get
+            {
+                if (_startCommand == null)
+                    {
+                    _startCommand = new StartCommand((param) =>
+                    {
+                        string minutesPattern = @"^(\d?\d{1}){1}", secondsPattern = @"([0-5]{1}\d{1}){1}$", minutes, seconds;
+                        minutes = Regex.Match(Time, minutesPattern).Value;
+                        seconds = Regex.Match(Time, secondsPattern).Value;
+                        timerObj.Start(minutes, seconds);
+                        IsEnable = false;
+                        Stop = false;
+                    }, (param) =>
+                    {
+                        string pattern = @"^(\d?\d{1}){1}:([0-5]{1}\d{1}){1}$";
+                        Regex regex = new Regex(pattern);
+                        Match match = regex.Match(Time);
+                        return match.Success;
+                    });
+                }
+                return _startCommand;
+            }
+        }
+        PauseCommand _pauseCommand;
+        public PauseCommand pauseCommand
+        {
+            get
+            {
+                if (_pauseCommand == null)
+                {
+                    _pauseCommand = new PauseCommand((param) =>
+                    {
+                        timerObj.Pause();
+                        IsEnable = !IsEnable;
+                    });
+                }
+                return _pauseCommand;
+            }
+        }
+        public void MyPrint(string timeValue)
+        {
+            Time = timeValue;
+        }
+    }
+    public class StartCommand : ICommand
+    {
+        protected Action<Exception> _error;//при объявлении можно задать обработчик ошибок. Гениально!
+        protected Action<Object> _execute;
+        protected Predicate<Object> _canExecute;
+        public StartCommand(Action<Object> execute, Predicate<Object> canExecute, Action<Exception> error=null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+            _error = error;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void Execute (object parameter)
+        {
+            try
+            {
+                _execute(parameter);//запуск таймера
+            }
+            catch(Exception ex)
+            {
+                if (_error != null)
+                    _error(ex);
+            }
+        }
+    }
+    public class PauseCommand : ICommand
+    {
+        protected Action<Exception> _error;//при объявлении можно задать обработчик ошибок. Гениально!
+        protected Action<Object> _execute;
+        public PauseCommand(Action<Object> execute, Action stop = null, Action<Exception> error = null)
+        {
+            _execute = execute;
+            _error = error;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public void Execute(object parameter)
+        {
+            try
+            {
+                _execute(parameter);
+            }
+            catch (Exception ex)
+            {
+                if (_error != null)
+                    _error(ex);
+            }
+        }
+    }
+}
---
+++ MyTimerClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTimer
+{
+    class MyTimerClass
+    {
+        Action<string> _print;        
+        Action _stop;
+        System.Timers.Timer MyTimer;
+        DateTime time;
+        Double secs;
+        public MyTimerClass(Action<string> Prnt = null, Action Stop = null)
+        {
+            MyTimer = new System.Timers.Timer(1000);
+            MyTimer.Enabled = false;
+            MyTimer.Elapsed += OnTimedEvent;
+            _stop = Stop;
+            _print = Prnt;
+        }
+        private void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
+        {
+            secs--;
+            if (secs >= 0)
+            {
+                time = time.AddSeconds(-1);
+                _print(string.Format("{0:m:ss}", time));
+            }
+            else
+            {
+                MyTimer.Stop();
+                _stop();
+            }
+        }
+        public void Start(string minutes, string seconds)
+        {
+            time = new DateTime();
+            time = time.AddMinutes(Double.Parse(minutes));
+            time = time.AddSeconds(Double.Parse(seconds));
+            secs = time.Second + time.Minute*60;
+            MyTimer.Start();
+        }
+        public void Pause ()
+        {
+            MyTimer.Enabled = !MyTimer.Enabled;
+        }
+    }
+}
